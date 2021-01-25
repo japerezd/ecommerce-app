a7bfdd66f713ff18afe9cddb4e7782e4db87d2ba
Nundu <jorge_alberto1997@hotmail.com>
2021-01-25 13:35:37 -0600

    Get device by slug. When click a product is redirect with its properties in SingleProduct component

--- src/components/pages/SingleProduct.js
+++ src/components/pages/SingleProduct.js
@@ -1,57 +1,56 @@
 import * as React from 'react';
-import image from "../../assets/images/smartphones/apple-iphone-12-mini-blanco-vista-frontal.webp";
-import image2 from "../../assets/images/smartphones/apple-iphone-12-mini-blanco-ambas-vistas.webp";
-import image3 from "../../assets/images/smartphones/apple-iphone-12-mini-blanco-vista-frontal-dinamica.webp";
-import image4 from "../../assets/images/smartphones/apple-iphone-12-mini-blanco-vista-trasera.webp";
+import { getDeviceBySlug } from '../../selectors/getDeviceBySlug';
 
 
-export const SingleProduct = () => {
+export const SingleProduct = ({match}) => {
+    const idPhone = match.params.idPhone;
+    const {price,images, description, battery, display, cpu, camera, name} = getDeviceBySlug(idPhone);
     return (
         <div className="product-details">
             <div className="product-image-container">
                 <div className="device-preview">
-                    <img src={image} alt='iphone'/>
+                    <img src={images[0].img1} alt='iphone'/>
                 </div>
 
                 <div className="device-preview__minipreviews">
                     <div className="device-preview__minipreviews--one">
-                        <img src={image} alt='iphone'/>
+                        <img src={images[0].img1} alt='iphone'/>
 
                     </div>
 
                     <div className="device-preview__minipreviews--two">
-                        <img src={image2} alt='iphone'/>
+                        <img src={images[1].im2} alt='iphone'/>
                     </div>
 
                     <div className="device-preview__minipreviews--three">
-                        <img src={image3} alt='iphone'/>
+                        <img src={images[2].img3} alt='iphone'/>
                     </div>
 
                     <div className="device-preview__minipreviews--four">
-                        <img src={image4} alt='iphone'/>
+                        <img src={images[3].img4} alt='iphone'/>
                     </div>
                 </div>
             </div>
 
             <div className="product-info">
-                <h1>Iphone bla bla</h1>
-                <h2>$price</h2>
+                <h1>{name}</h1>
+                <h2>${price}</h2>
 
                 <p> <strong>Description</strong> 
               
                 </p>
-                <span>Lorem Ipsum Dolor Sit Amet, Consectetur Adipisicing Elit. Amet Numquam </span>
+                <span> {description} </span>
                 <p> <strong>Battery</strong>  </p>
-                <span>5020 mAh</span>
+                <span> {battery} </span>
 
                 <p> <strong>Display</strong></p>
-                <span>6.67"</span>
+                <span> {display} </span>
 
                 <p> <strong>CPU</strong> </p>
-                <span>SnapDragon 720G</span>
+                <span> {cpu} </span>
 
                 <p> <strong>Camera</strong> </p>
-                <span>48 + 8 + 5 + 2 MP</span>
+                <span> {camera} </span>
 
                 <div className="cart-button__wrapper">
                  Add to cart
